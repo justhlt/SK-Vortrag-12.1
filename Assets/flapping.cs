24175035b8d8e5ed28567969da85aa6170e5cd1c
klarinsteffens <klarin.steffens@outlook.de>
2024-12-13 06:49:07 +0100

    jkhlhblkj hliu

---
+++ Assets/flapping.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flapping : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody2D;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
